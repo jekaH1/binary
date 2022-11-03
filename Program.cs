a04af7bb14e081c2bedfc5d92e12663b06de9197
jekaH1 <ceyhun100203@gmail.com>
2022-11-03 12:08:16 +0400

    added

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 7, 12, 34, 54, 67, 89, 90 };
+            Console.WriteLine("eded daxil edin");
+            int eded = Convert.ToInt32(Console.ReadLine());
+            int left = 0;
+            int right = arr.Length-1 ;
+            int mid = default(int);
+
+
+            while(left <= right)
+            {
+                mid = (left+right)/2 ;
+                if (arr[mid] == eded) 
+                {
+                    Console.WriteLine("eded " + mid + "-ci indexdedir");
+                    break;
+                }
+                else if (arr[mid] >= eded)
+                {
+                    right = mid - 1;
+                }
+                else if (arr[mid] <= eded)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    Console.WriteLine("bele indexli eded yoxdur");
+                }
+            }
+                        
+        }
+    }
+}
